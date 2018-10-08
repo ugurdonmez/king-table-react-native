69e15bcd8fe63bafe82ef3d463be9bff177d1e96
Ugur Donmez <ugurdonmez87@gmail.com>
2018-10-08 21:34:40 +0200

    add + button to game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 import Name from '../components/name/Name';
 import HandNane from '../components/handName/HandName';
 import HandScore from '../components/handScore/HandScore';
@@ -7,14 +7,41 @@ import HandScore from '../components/handScore/HandScore';
 class Game extends Component {
 
     static navigationOptions = ({ navigation }) => ({
-        title: `${navigation.state.params.title}`,
+        // title: `${navigation.state.params.title}`,
+        headerTitle: 'Yeni Oyun',
         headerTitleStyle: { textAlign: 'center', alignSelf: 'center' },
         headerStyle: {
             backgroundColor: 'white',
         },
+        headerRight: (
+            <Button
+                style={styles.addButtonTitle}
+                title='+'
+                onPress={() => console.log('pressed')}
+            />
+            
+        ),
     });
 
     render() {
+
+        let rows = []
+
+        for (let i = 0; i < 20; i++) {
+            rows.push(
+            <View
+                key={i}
+                style={{
+                flexDirection: 'row',
+            }}>
+                <HandNane />
+                <HandScore />
+                <HandScore />
+                <HandScore />
+                <HandScore />
+            </View>)
+        }
+
         return (
             <View>
                 <View style={{
@@ -27,18 +54,16 @@ class Game extends Component {
                     <Name />
                 </View>
 
-                <View style={{
-                    flexDirection: 'row',
-                }}>
-                    <HandNane />
-                    <HandScore />
-                    <HandScore />
-                    <HandScore />
-                    <HandScore />
-                </View>
+                {rows}
             </View>
         )
     }
 }
 
 export default Game
+
+var styles = StyleSheet.create({
+    addButtonTitle: {
+        fontSize: 16,
+    }
+})
