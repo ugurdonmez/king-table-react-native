494ef0b4e2423f9e60732976265c86b3ebb273c2
Ugur Donmez <ugurdonmez87@gmail.com>
2018-10-09 21:52:19 +0200

    enzyme does not work

--- src/components/name/Name.test.js
+++ src/components/name/Name.test.js
@@ -2,6 +2,13 @@ import 'react-native';
 import React from 'react';
 import Name from './Name';
 
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import 'jest-enzyme'
+import React from 'react'
+Enzyme.configure({adapter: new Adapter()}) // ????
+
+
 import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
@@ -12,4 +19,18 @@ it('renders correctly', () => {
     console.log(tree);
 
     expect(tree).toMatchSnapshot();
+})
+
+it('check propbs', () => {
+    const wrapper = shallow(
+        <Name 
+            name="test"
+            punish="2"
+            reward="1"
+        />
+    )
+
+    expect(wrapper).toMatchSnapshot();
+    
+
 })
