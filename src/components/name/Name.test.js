ee29f52cd0c9435af51d16ac23bb8e2041fda805
Ugur Donmez <ugurdonmez87@gmail.com>
2018-10-10 23:20:09 +0200

    fix enzme error

--- src/components/name/Name.test.js
+++ src/components/name/Name.test.js
@@ -1,15 +1,14 @@
-import 'react-native';
 import React from 'react';
+import 'react-native';
 import Name from './Name';
-
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import 'jest-enzyme'
-import React from 'react'
-Enzyme.configure({adapter: new Adapter()}) // ????
-
+import { shallow } from 'enzyme'
 
 import renderer from 'react-test-renderer'
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders correctly', () => {
     const tree = renderer.create(
@@ -29,8 +28,5 @@ it('check propbs', () => {
             reward="1"
         />
     )
-
     expect(wrapper).toMatchSnapshot();
-    
-
 })
