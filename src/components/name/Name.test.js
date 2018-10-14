b683948bd80561420d0d8960fca2ceb896e9049f
Ugur Donmez <ugurdonmez87@gmail.com>
2018-10-14 22:54:38 +0200

    name component tests

--- src/components/name/Name.test.js
+++ src/components/name/Name.test.js
@@ -4,21 +4,41 @@ import Name from './Name';
 import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
-    const tree = renderer.create(
-        <Name />
-    ).toJSON();
-    
-    expect(tree).toMatchSnapshot();
-})
+describe('<Name /> tests', () => {
+    // it('renders without crashing', () => {
+    //     shallow(
+    //         <Name 
+    //             name="test"
+    //             punish="2"
+    //             reward="1"
+    //         />)
+    // })
 
-it('check propbs', () => {
-    const wrapper = shallow(
-        <Name 
-            name="test"
-            punish="2"
-            reward="1"
-        />
-    )
-    expect(wrapper).toMatchSnapshot();
+    // it('shows correct name', () => {
+    //     const wrapper = shallow(
+    //         <Name 
+    //             name="test"
+    //             punish="2"
+    //             reward="1"
+    //         />
+    //     );
+    
+    //     wrapper.find('Text').forEach(child => {
+    //         expect(child.children().text()).toBe('test');
+    //     })
+    // })
+
+    it('should show 2 empty triangle and 3 empty circle', () => {
+        const wrapper = shallow(
+            <Name 
+                name="test"
+                punish="0"
+                reward="0"
+            />
+        )
+
+        wrapper.find('Component').forEach(child => {
+            console.log(child.text())
+        })
+    })
 })
