dcda4e99be07b0baeb5f6883956c160c7fb7d437
Ugur Donmez <ugurdonmez87@gmail.com>
2018-10-15 22:50:29 +0200

    name component tests

--- src/components/name/Name.test.js
+++ src/components/name/Name.test.js
@@ -5,28 +5,28 @@ import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
 describe('<Name /> tests', () => {
-    // it('renders without crashing', () => {
-    //     shallow(
-    //         <Name 
-    //             name="test"
-    //             punish="2"
-    //             reward="1"
-    //         />)
-    // })
+    it('renders without crashing', () => {
+        shallow(
+            <Name 
+                name="test"
+                punish="2"
+                reward="1"
+            />)
+    })
 
-    // it('shows correct name', () => {
-    //     const wrapper = shallow(
-    //         <Name 
-    //             name="test"
-    //             punish="2"
-    //             reward="1"
-    //         />
-    //     );
+    it('shows correct name', () => {
+        const wrapper = shallow(
+            <Name 
+                name="test"
+                punish="2"
+                reward="1"
+            />
+        );
     
-    //     wrapper.find('Text').forEach(child => {
-    //         expect(child.children().text()).toBe('test');
-    //     })
-    // })
+        wrapper.find('Text').forEach(child => {
+            expect(child.children().text()).toBe('test');
+        })
+    })
 
     it('should show 2 empty triangle and 3 empty circle', () => {
         const wrapper = shallow(
@@ -35,10 +35,80 @@ describe('<Name /> tests', () => {
                 punish="0"
                 reward="0"
             />
-        )
+        );
 
-        wrapper.find('Component').forEach(child => {
-            console.log(child.text())
+        const components = wrapper.find('Component');
+
+        expect(components.length).toBe(3);
+
+        // second component for reward, two empty triangle
+        expect(components.at(1).children().length).toBe(2);
+
+        components.at(1).children().forEach(child => {
+            expect(child.prop('name')).toBe('TriangleEmpty')
+        })
+
+        // third component for punish, three empty circle 
+        expect(components.at(2).children().length).toBe(3);
+
+        components.at(2).children().forEach(child => {
+            expect(child.prop('name')).toBe('CircleEmpty')
+        })
+    })
+
+    it('reward 1, punish 1 ', () => {
+        const wrapper = shallow(
+            <Name 
+                name="test"
+                reward="1"
+                punish="1"
+            />
+        );
+
+        const components = wrapper.find('Component');
+
+        expect(components.length).toBe(3);
+
+        // second component for reward
+        expect(components.at(1).children().length).toBe(2);
+
+        components.at(1).children().at(0).prop('name').toBe('Triangle')
+        components.at(1).children().at(1).prop('name').toBe('TriangleEmpty')
+        
+
+        // third component for punish
+        expect(components.at(2).children().length).toBe(3);
+
+        components.at(2).children().at(0).prop('name').toBe('Circle')
+        components.at(2).children().at(1).prop('name').toBe('CircleEmpty')
+        components.at(2).children().at(2).prop('name').toBe('CircleEmpty')
+    })
+
+    it('should show 2 triangle and 3 circle', () => {
+        const wrapper = shallow(
+            <Name 
+                name="test"
+                reward="2"
+                punish="3"
+            />
+        );
+
+        const components = wrapper.find('Component');
+
+        expect(components.length).toBe(3);
+
+        // second component for reward, two empty triangle
+        expect(components.at(1).children().length).toBe(2);
+
+        components.at(1).children().forEach(child => {
+            expect(child.prop('name')).toBe('Triangle')
+        })
+
+        // third component for punish, three empty circle 
+        expect(components.at(2).children().length).toBe(3);
+
+        components.at(2).children().forEach(child => {
+            expect(child.prop('name')).toBe('Circle')
         })
     })
 })
